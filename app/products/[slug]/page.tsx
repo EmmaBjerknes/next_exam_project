79ffc1afb89bbdbe5107fde619a5db294d5ddf40
Emma Bjerknes <emma.bjerknes@mio.se>
2024-03-19 16:27:34 +0100

    initial commit

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+// products/id
+const singleProduct = () => {
+  return <div>singleProduct</div>;
+};
+
+export default singleProduct;
---
+++ app/signin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const signIn = () => {
+  return <div>Sign in</div>;
+};
+
+export default signIn;
