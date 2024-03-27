4d6b358000778816e4d044ebc833a033f6e2463b
Emma Bjerknes <emma.bjerknes@mio.se>
2024-03-27 10:50:09 +0100

    removed unused pages

--- app/signin/createaccount/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const createAccount = () => {
-  return <div>createAccount</div>;
-};
-
-export default createAccount;
--- app/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const signIn = () => {
-  return <div>Sign in</div>;
-};
-
-export default signIn;
