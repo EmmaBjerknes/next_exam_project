ad215410e31acb2ac9549bf1b1efb9da28e8851e
Emma Bjerknes <emma.bjerknes@mio.se>
2024-03-27 12:59:35 +0100

    resolved build fail v2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
+const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
