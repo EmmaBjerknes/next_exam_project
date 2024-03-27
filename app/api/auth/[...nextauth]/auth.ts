b335a46db9b3efee76af5fd1fed05fb81c18c3a4
Emma Bjerknes <emma.bjerknes@mio.se>
2024-03-27 12:42:58 +0100

    resolved build fail

---
+++ app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,11 @@
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
