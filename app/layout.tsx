2f9059ebc962248f00258e0bf324e60e57338fe7
Emma Bjerknes <emma.bjerknes@mio.se>
2024-03-27 13:47:58 +0100

    removed server component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { Inter } from "next/font/google";
 import "./globals.css";
 import MainHeader from "./components/MainHeader";
 import Footer from "./components/Footer";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -19,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SessionProvider session={session}>
-        <body className={inter.className}>
+        <body>
           <MainHeader />
           <main className="flex min-h-screen flex-col items-center justify-between p-24">
             {children}
