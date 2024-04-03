e47395b76741fb473a08e7ace6665f0f970ba9e7
Emma Bjerknes <emma.bjerknes@mio.se>
2024-04-03 12:54:01 +0200

    remove depricated code

--- app/data/navigationData.ts
+++
@@ -1,7 +0,0 @@
-export const navigationData = [
-    { name: "Home", path: "/" },
-    { name: "Products", path: "/products" },
-    { name: "Categories", path: "#" },
-    { name: "Campaign", path: "/products/campaign" },
-    { name: "About", path: "/about" },
-];
