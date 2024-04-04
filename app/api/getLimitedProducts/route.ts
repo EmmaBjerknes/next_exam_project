b892a923d6c0578bd3657c124c7823e087f66923
Emma Bjerknes <emma.bjerknes@mio.se>
2024-04-04 09:42:05 +0200

    implement new GET

---
+++ app/api/getLimitedProducts/route.ts
@@ -0,0 +1,39 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const limitedProductList = await sql`
+      SELECT 
+        p.name,
+        p.description,
+        p.productimage,
+        p.price,
+        p.category,
+        c.name AS campaign_name,
+        c.discountPercent AS campaign_discount_percent
+      FROM 
+        products p
+      LEFT JOIN 
+        campaign c ON p.campaign = c.id
+      LIMIT 14
+    `;
+    const validProducts = limitedProductList.rows.filter((product) => {
+      return (
+        product.productimage.includes("/images/products/") &&
+        product.productimage.endsWith(".jpg")
+      );
+    });
+    return NextResponse.json(validProducts, { status: 200 });
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+    throw new Error("Failed to fetch products.");
+  }
+}
+
+// ISSUE: För tillfället finns det trasiga URL:er i databasen
+// TODO:  Säkerställ att bara valida URL:er läggs in av admin
+//        Då kan man sänka limit till 7 här och ta bort valideringen
+//
+// Nice to have: Förhandsvisning av bilden på admin-sidan
+//              - ger feedback visuellt till admin
