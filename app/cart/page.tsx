e34fb4c4aff3b55cdf590991ff417c1fc35f5a2d
Emma Bjerknes <emma.bjerknes@mio.se>
2024-04-16 10:00:03 +0200

    add cartlist for items in cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import CartList from "../components/Cart/CartList";
 
-const cart = () => {
-  return <div>cart</div>;
+const Cart = () => {
+  return (
+    <div className="flex flex-col gap-8 p-4 mt-2 md:mt-8 w-full justify-around md:flex-row">
+      <CartList />
+      <div>Prisberäkning</div>
+    </div>
+  );
 };
 
-export default cart;
+export default Cart;
